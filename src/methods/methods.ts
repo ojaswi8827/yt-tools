7e089dfd0df61f857ae98943ff1ad6e966eeb77e
Ojaswi <ojaswi@vahak.in>
2023-02-08 21:13:23 +0530

    Updates

--- src/methods/methods.ts
+++
@@ -1,4 +0,0 @@
-export const addHashtags = (text: string) =>
-  "#" + text.replaceAll(" ", "").replaceAll("\n", " #").trim();
-
-export const addCommas = (text: string) => text.replaceAll("\n", ", ") + ",";
